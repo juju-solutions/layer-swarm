a4e8603484953017f342944983e59267dc795e8b
Charles Butler <chuck@dasroot.net>
2016-03-20 04:45:56 -0400

    Initial support for consul

--- reactive/swarm.py
+++ reactive/swarm.py
@@ -3,17 +3,15 @@ from charmhelpers.core import host
 from charmhelpers.core.templating import render
 from charms.reactive import when
 from charms.reactive import when_not
+from charms.reactive import when_file_changed
+from charms.reactive import remove_state
 from charms import reactive
 
-from charms.docker import Docker
 from charms.docker.dockeropts import DockerOpts
 from charms.docker.compose import Compose
 
 from charmhelpers.core.hookenv import status_set
 from charmhelpers.core.hookenv import is_leader
-
-from shlex import split
-from subprocess import check_call
 
 
 @when('etcd.available', 'docker.available')
@@ -34,11 +32,22 @@ def swarm_etcd_cluster_setup(etcd):
     opts['leader'] = is_leader()
 
     render('docker-compose.yml', 'files/swarm/docker-compose.yml', opts)
-    compose = Compose('files/swarm')
-    compose.up()
-    hookenv.open_port(2376)
-    reactive.set_state('swarm.available')
-    hookenv.status_set('active', 'Swarm configured. Happy swarming')
+
+
+@when('consul.available', 'docker.available')
+@when_not('swarm.available')
+def swarm_consul_cluster_setup(consul):
+    bind_docker_daemon()
+    hodor = []
+    for unit in consul.list_unit_data():
+        hodor.append("{}:{}".format(unit['address'], unit['port']))
+    # only use the first while we test
+    opts = {}
+    opts['addr'] = hookenv.unit_private_ip()
+    opts['port'] = 2375
+    opts['leader'] = is_leader()
+    opts['connection_string'] = "consul://{}".format(hodor[0])
+    render('docker-compose.yml', 'files/swarm/docker-compose.yml', opts)
 
 
 @when('swarm.available')
@@ -49,17 +58,17 @@ def swarm_messaging():
         status_set('active', 'Swarm follower')
 
 
-@when_not('etcd.connected')
+@when_not('etcd.connected', 'consul.connected')
 def user_notice():
     """
-    Notify the user they need to relate the charm with ETCD to trigger the
-    swarm cluster configuration.
+    Notify the user they need to relate the charm with ETCD or Consul to
+    trigger the swarm cluster configuration.
     """
-    hookenv.status_set('blocked', 'Pending ETCD connection for swarm')
+    hookenv.status_set('waiting', 'Waiting on Etcd or Consul connection')
 
 
 @when('swarm.available')
-@when_not('etcd.connected')
+@when_not('etcd.connected', 'consul.connected')
 def swarm_relation_broken():
     """
     Destroy the swarm agent, and optionally the manager.
@@ -71,6 +80,16 @@ def swarm_relation_broken():
     c.rm()
     remove_state('swarm.available')
     status_set('waiting', 'Reconfiguring swarm')
+
+
+@when_file_changed('files/swarm/docker-compose.yml')
+def start_swarm():
+    compose = Compose('files/swarm')
+    compose.up()
+    hookenv.open_port(2376)
+    reactive.set_state('swarm.available')
+    hookenv.status_set('active', 'Swarm configured. Happy swarming')
+
 
 def bind_docker_daemon():
     hookenv.status_set('maintenance', 'Configuring Docker for TCP connections')
