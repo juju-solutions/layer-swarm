976b9f616afb3ebfc6aea3132a99223b0000eb42
Charles Butler <chuck@dasroot.net>
2016-04-27 12:25:02 -0400

    Rebase with master

--- reactive/swarm.py
+++ reactive/swarm.py
@@ -3,6 +3,8 @@ from charmhelpers.core import host
 from charmhelpers.core.templating import render
 from charms.reactive import when
 from charms.reactive import when_not
+from charms.reactive import when_file_changed
+from charms.reactive import remove_state
 from charms import reactive
 
 from charms.docker import Docker
@@ -58,6 +60,22 @@ def swarm_etcd_cluster_setup(etcd):
     hookenv.status_set('active', 'Swarm configured. Happy swarming')
 
 
+@when('consul.available', 'docker.available')
+@when_not('swarm.available')
+def swarm_consul_cluster_setup(consul):
+    hodor = []
+    for unit in consul.list_unit_data():
+        hodor.append("{}:{}".format(unit['address'], unit['port']))
+    # only use the first while we test
+    opts = {}
+    opts['addr'] = hookenv.unit_private_ip()
+    opts['port'] = 2375
+    opts['leader'] = is_leader()
+    opts['connection_string'] = "consul://{}".format(hodor[0])
+    render('docker-compose.yml', 'files/swarm/docker-compose.yml', opts)
+    bind_docker_daemon(opts['connection_string'])
+    reactive.set_state('restart-docker')
+
 @when('restart-docker')
 def restart_docker_service():
     host.service_restart('docker')
@@ -71,17 +89,17 @@ def swarm_messaging():
         status_set('active', 'Swarm follower')
 
 
-@when_not('etcd.connected')
+@when_not('etcd.connected', 'consul.connected')
 def user_notice():
     """
-    Notify the user they need to relate the charm with ETCD to trigger the
-    swarm cluster configuration.
+    Notify the user they need to relate the charm with ETCD or Consul to
+    trigger the swarm cluster configuration.
     """
-    hookenv.status_set('blocked', 'Pending ETCD connection for swarm')
+    hookenv.status_set('waiting', 'Waiting on Etcd or Consul relation')
 
 
 @when('swarm.available')
-@when_not('etcd.connected')
+@when_not('etcd.connected', 'consul.connected')
 def swarm_relation_broken():
     """
     Destroy the swarm agent, and optionally the manager.
@@ -186,7 +204,8 @@ def prepare_package():
         reactive.set_state('client.credentials.placed')
 
 
-def bind_docker_daemon(connection_string):
+
+def bind_docker_daemon():
     hookenv.status_set('maintenance', 'Configuring Docker for TCP connections')
     opts = DockerOpts()
     private_address = hookenv.unit_private_ip()
