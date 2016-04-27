4e2fbf12d6597ddc53cd644b907144aedbb17be9
Charles Butler <chuck@dasroot.net>
2016-04-27 09:03:41 -0400

    SSL Termination for the Docker daemon and Swarm managers

--- reactive/swarm.py
+++ reactive/swarm.py
@@ -6,15 +6,26 @@ from charms.reactive import when_not
 from charms import reactive
 
 from charms.docker import Docker
-from charms.docker.dockeropts import DockerOpts
-from charms.docker.compose import Compose
+from charms.docker import DockerOpts
+from charms.docker import Compose
 
 from charmhelpers.core.hookenv import log
 from charmhelpers.core.hookenv import status_set
 from charmhelpers.core.hookenv import is_leader
+from charmhelpers.core.hookenv import leader_get
+from charmhelpers.core.hookenv import unit_get
+from charmhelpers.core import unitdata
 
+from os import path
+from os import makedirs
+from os import getenv
+
+import subprocess
 from shlex import split
-from subprocess import check_call
+from shutil import copyfile
+
+from tlslib import client_cert
+from tlslib import ca
 
 
 @when('etcd.available', 'docker.available')
@@ -25,14 +36,15 @@ def swarm_etcd_cluster_setup(etcd):
     leading with the agent, connecting to the discovery service, then follow
     up with the manager container on the leader node.
     """
-    bind_docker_daemon()
     con_string = etcd.connection_string().replace('http', 'etcd')
+    bind_docker_daemon(con_string)
 
     opts = {}
     opts['connection_string'] = con_string
     opts['addr'] = hookenv.unit_private_ip()
-    opts['port'] = 2375
+    opts['port'] = 2376
     opts['leader'] = is_leader()
+    opts['charm_dir'] = getenv('CHARM_DIR')
 
     render('docker-compose.yml', 'files/swarm/docker-compose.yml', opts)
     compose = Compose('files/swarm')
@@ -41,6 +53,11 @@ def swarm_etcd_cluster_setup(etcd):
     reactive.set_state('swarm.available')
     hookenv.status_set('active', 'Swarm configured. Happy swarming')
 
+
+@when('restart-docker')
+def restart_docker_service():
+    host.service_restart('docker')
+    reactive.remove_state('restart-docker')
 
 @when('swarm.available')
 def swarm_messaging():
@@ -73,13 +90,6 @@ def swarm_relation_broken():
     remove_state('swarm.available')
     status_set('waiting', 'Reconfiguring swarm')
 
-def bind_docker_daemon():
-    hookenv.status_set('maintenance', 'Configuring Docker for TCP connections')
-    opts = DockerOpts()
-    opts.add('host', 'tcp://{}:2375'.format(hookenv.unit_private_ip()))
-    opts.add('host', 'unix:///var/run/docker.sock')
-    render('docker.defaults', '/etc/default/docker', {'opts': opts.to_s()})
-    host.service_restart('docker')
 
 @when('easyrsa installed')
 @when_not('swarm.tls.opensslconfig.modified')
@@ -87,7 +97,7 @@ def inject_swarm_tls_template():
     """
     layer-tls installs a default OpenSSL Configuration that is incompatibile
     with how swarm expects TLS keys to be generated. We will append what
-    we need to the TLS config, and let layer-tls take over from there.
+    we need to the x509-type, and poke layer-tls to regenerate whatever is there.
     """
     if not is_leader():
         return
@@ -95,23 +105,79 @@ def inject_swarm_tls_template():
         status_set('maintenance', 'Reconfiguring SSL PKI configuration')
 
     log('Updating EasyRSA3 OpenSSL Config')
-    openssl_config = 'easy-rsa/easyrsa3/openssl-1.0.cnf'
+    openssl_config = 'easy-rsa/easyrsa3/x509-types/server'
+
     with open(openssl_config, 'r') as f:
         existing_template = f.readlines()
 
-    for idx, line in enumerate(existing_template):
-        if '[ req ]' in line:
-            existing_template.insert(idx + 1, "req_extensions = v3_req\n")
+    # use list comprehension to enable clients,server usage for certificates
+    # with the docker/swarm daemons.
+    xtype = [w.replace('serverAuth', 'serverAuth, clientAuth') for w in existing_template]  # noqa
+    with open (openssl_config, 'w+') as f:
+        f.writelines(xtype)
 
-    v3_reqs = ['[ v3_req ]\n',
-    'basicConstraints = CA:FALSE\n',
-    'keyUsage = nonRepudiation, digitalSignature, keyEncipherment\n',
-    'extendedKeyUsage = clientAuth, serverAuth\n']
-
-    swarm_ssl_config = existing_template + v3_reqs
-
-    with open(openssl_config, 'w') as f:
-        for line in swarm_ssl_config:
-            f.write(line)
     reactive.set_state('swarm.tls.opensslconfig.modified')
-    reactive.set_state('easyrsa configured')
+    reactive.set_state('easyrsa reconfigured')
+
+
+@when('tls.server.certificate available')
+def enable_client_tls():
+    '''
+    Copy the TLS certificates in place and generate mount points for the swarm
+    manager to mount the certs. This enables client-side TLS security on the
+    TCP service.
+    '''
+    if not path.exists('/etc/docker'):
+        makedirs('/etc/docker')
+
+    kv = unitdata.kv()
+    cert = kv.get('tls.server.certificate')
+    with open('/etc/docker/server.pem', 'w+') as f:
+        f.write(cert)
+    with open('/etc/docker/ca.pem', 'w+') as f:
+        f.write(leader_get('certificate_authority'))
+
+    # schenanigans
+    keypath = 'easy-rsa/easyrsa3/pki/private/{}.key'
+    server = getenv('JUJU_UNIT_NAME').replace('/', '_')
+    if path.exists(keypath.format(server)):
+        copyfile(keypath.format(server), '/etc/docker/server-key.pem')
+    else:
+        copyfile(keypath.format(unit_get('public-address')), '/etc/docker/server-key.pem')
+
+    opts = DockerOpts()
+    config_dir = '/etc/docker'
+    cert_path = '{}/server.pem'.format(config_dir)
+    ca_path = '{}/ca.pem'.format(config_dir)
+    key_path = '{}/server-key.pem'.format(config_dir)
+    opts.add('tlscert', cert_path)
+    opts.add('tlscacert', ca_path)
+    opts.add('tlskey', key_path)
+    opts.add('tlsverify', None)
+    render('docker.defaults', '/etc/default/docker', {'opts': opts.to_s()})
+
+
+@when('swarm.available')
+@when_not('client.credentials.placed')
+def prepare_package():
+    """ Generate a downloadable package for clients to use to speak to the
+    swarm cluster. """
+    if is_leader():
+        client_cert('./swarm_credentials')
+        ca('./swarm_credentials')
+        cmd = 'tar cvf swarm_credentials.tar swarm_credentials'
+        subprocess.check_call(split(cmd))
+        copyfile('swarm_credentials.tar', '/home/ubuntu/swarm_credentials.tar')
+        reactive.set_state('client.credentials.placed')
+
+
+def bind_docker_daemon(connection_string):
+    hookenv.status_set('maintenance', 'Configuring Docker for TCP connections')
+    opts = DockerOpts()
+    private_address = hookenv.unit_private_ip()
+    opts.add('host', 'tcp://{}:2376'.format(private_address))
+    opts.add('host', 'unix:///var/run/docker.sock')
+    opts.add('cluster-advertise', '{}:2376'.format(private_address))
+    opts.add('cluster-store', connection_string)
+    render('docker.defaults', '/etc/default/docker', {'opts': opts.to_s()})
+    reactive.set_state('restart-docker')
